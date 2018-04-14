bd5f401bffac4c9675f314f7b48543746a46e396
John Matthews <johnmatthews2634@gmail.com>
2018-04-14 12:37:14 -0700

    add root.jsx and app.jsx

---
+++ trello_clone/frontend/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider, Redirect } from 'react-redux';
+import {
+  Route,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+
+
+const App = () => (
+  <div className="mainDiv">
+      
+  </div>
+);
+
+export default App;
